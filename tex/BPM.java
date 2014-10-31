39d0006e15bbd33078994f089244dd5928d69896
Malte Skambath <malte@skambath.de>
2014-10-31 02:03:13 +0100

    added BPM

---
+++ tex/BPM.java
@@ -0,0 +1,66 @@
+import java.util.*;
+
+
+public class BPM {
+	 int m, n;
+	    boolean[][] graph;
+	    boolean seen[];
+	    int matchL[];   //What left vertex i is matched to (or -1 if unmatched)
+	    int matchR[];   //What right vertex j is matched to (or -1 if unmatched)
+	 
+	    int maximumMatching() {
+	        //Read input and populate graph[][]
+	        //Set m to be the size of L, n to be the size of R
+	        Arrays.fill(matchL, -1);
+	        Arrays.fill(matchR, -1);
+	 
+	        int count = 0;
+	        for (int i = 0; i < m; i++) {
+	            Arrays.fill(seen, false);
+	            if (bpm(i)) count++;
+	        }
+	        return count;
+	    }
+	 
+	    boolean bpm(int u) {
+	        //try to match with all vertices on right side
+	        for (int v = 0; v < n; v++) {
+	            if (!graph[u][v] || seen[v]) continue;
+	            seen[v] = true;
+	            //match u and v, if v is unassigned, or if v's match on the left side can be reassigned to another right vertex
+	            if (matchR[v] == -1 || bpm(matchR[v])) {
+	                matchL[u] = v;
+	                matchR[v] = u;
+	                return true;
+	            }
+	        }
+	        return false;
+	    }
+	    
+	    public void run(){
+	    	
+	    	Scanner sc = new Scanner(System.in).useLocale(Locale.US);
+	    	int T = sc.nextInt();
+	    	while(T-->0){
+	    		n = sc.nextInt();
+	    		m =  sc.nextInt();
+	    		int K = sc.nextInt();
+	    		graph = new boolean [m][n];
+	    		matchL = new int[m];
+	    		matchR = new int[n];
+	    		seen = new boolean[n];
+	    		while(K-->0){
+	    			int y = (int)sc.nextDouble();
+	    			int x  = (int)sc.nextDouble();
+	    			graph[x][y] = true;
+	    			
+	    		}
+	    		System.out.println(maximumMatching());
+	    	}
+	    	sc.close();
+	    }
+	    
+	    public static void main(String[] args){
+	    	(new BPM()).run();
+	    }
+}
